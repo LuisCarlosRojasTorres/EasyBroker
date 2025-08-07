a33de8776d91c4d871d46f9d46ae8542ad9649e5
LuisCarlosRojasTorres <redtower.soft@gmail.com>
2025-08-07 06:58:40 -0300

    Added IEasyMqttClient

---
+++ EasyBroker/EasyMqttLib/Interfaces/IEasyMqttClient.cs
@@ -0,0 +1,22 @@
+﻿namespace EasyMqttLib.Interfaces;
+
+using MQTTnet.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Interface for MqttClient
+/// </summary>
+public interface IEasyMqttClient
+{    
+    public Task ConnectToBroker();
+    public Task DisconnectToBroker();
+
+    public Task SubscribeToTopics(List<string> Topics);
+    public Task UnsubscribeToTopics(List<string> Topics);
+
+    public Task PublishTopic(string topic, string message, bool isRetainedMessage);
+}
